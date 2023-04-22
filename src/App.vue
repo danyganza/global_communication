d07d51bc0807ad76cb369bc259c0c492e801aec3
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-22 09:34:11 -0600

    added message to event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<toast-notification></toast-notification>
+<delete-button></delete-button>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToastNotification from "@/components/ToastNotification.vue"
+import DeleteButton from "@/components/DeleteButton.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   ToastNotification,
+   DeleteButton
   }
 }
 </script>
---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <button @click="toast_notify">DELETE</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toast_notify() {
+      // Emit the custom event
+      this.$root.$emit('toast_notify', { message: 'Item deleted successfully' });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
